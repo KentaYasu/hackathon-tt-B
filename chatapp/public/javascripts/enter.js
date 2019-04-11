889eada3f06631437cd39c3d1191c48ad40329bc
ShimaRino <donguriko0223@gmail.com>
2019-04-11 20:07:47 +0900

    committed in 2019/04/11 20:07:47

--- chatapp/public/javascripts/enter.js
+++ chatapp/public/javascripts/enter.js
@@ -4,9 +4,9 @@
 // 入力されたユーザ名を取得する
 const userName = $('#userName').val();
 // 入室メッセージイベントを送信する
-
+socket.emit('enter',userName)
 
 // サーバから受信した入室メッセージを画面上に表示する
-socket.on('', function (data) {
-    $('#thread').prepend('<p>' + '</p>');
+socket.on('enterOtherEvent', function (data) {
+    $('#thread').prepend('<p>' + data + '</p>');
 });
