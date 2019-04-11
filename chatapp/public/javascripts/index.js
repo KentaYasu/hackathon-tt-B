be8073f92c8a00701152544b938158be973f2e6e
hrt-ktmr <snow.longa.whi.t.ang.el@gmail.com>
2019-04-11 20:03:35 +0900

    committed in 2019/04/11 20:03:35

--- chatapp/public/javascripts/index.js
+++ chatapp/public/javascripts/index.js
@@ -1,11 +1,20 @@
 'use strict';
 
+function getValue(){
+    const userName = $('#userName').val();
+    return userName;
+}
+
 // チャットルームに入室する
 function enter() {
     // 入力されたユーザ名を取得する
-    const userName = '';
+    const userName = getValue();
+    if (userName === ''){
+        alert("ユーザー名が入力されていません");
+    }else{
     // ユーザ名が未入力でないかチェックする
 
 
-    $('form').submit();
+        $('form').submit();
+    }
 }
