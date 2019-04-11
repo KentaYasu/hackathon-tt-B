19565dc4cfead96b0f05e66ae949f0debc979845
ktakasak1 <ktksk2@gmail.com>
2019-04-11 21:06:02 +0900

    committed in 2019/04/11 21:06:02

--- chatapp/public/javascripts/publish.js
+++ chatapp/public/javascripts/publish.js
@@ -1,24 +1,33 @@
 'use strict';
 
+let preResponceDate
+
 // 投稿メッセージをサーバに送信する
 function publish() {
+    //１分間ボタンを押せなくする
+    disableButtonMinute();
+
     // ユーザ名を取得
     const userName = $('#userName').val();
     // 入力されたメッセージを取得
     const message = $('#message').val();
-    if(message !== ''){
     // 投稿内容を送信
-        socket.emit('publish', {userName: userName, message: message});
-    }else{
-        alert("空白では送信できません");
-    }
-return false;
+    socket.emit('publish', {userName: userName, message: message})
+
+    return false;
 }
 
 function enterKeyPressed() {
   if (window.event.keyCode == 13) {
     publish();
   }
+}
+
+function disableButtonMinute(){
+  $('#publish-btn').attr('disabled', true);
+  setTimeout(() => {
+      $('#publish-btn').attr('disabled', false);
+  }, 60000);
 }
 
 // サーバから受信した投稿メッセージを画面上に表示する
