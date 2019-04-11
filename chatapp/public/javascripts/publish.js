41ee64e6bac03e69a5e1132bc20d1c375c2851c7
hrt-ktmr <snow.longa.whi.t.ang.el@gmail.com>
2019-04-11 20:40:32 +0900

    committed in 2019/04/11 20:40:32

--- chatapp/public/javascripts/publish.js
+++ chatapp/public/javascripts/publish.js
@@ -3,13 +3,16 @@
 // 投稿メッセージをサーバに送信する
 function publish() {
     // ユーザ名を取得
-    const userName = $('#userName').val();
+    const userName = $('#userName'.val) ;
     // 入力されたメッセージを取得
-    const message = $('#message').val();
+    const message = $('#message'.val);
+    if(message !== ''){
     // 投稿内容を送信
-    socket.emit('publish', {userName: userName, message: message})
-
-    return false;
+        socket.emit('publish',message);
+    }else{
+        alert("空白では送信できません");
+    }
+return false;
 }
 
 // サーバから受信した投稿メッセージを画面上に表示する
